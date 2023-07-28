00031fd71b81e6d6e704bba91461d6a8b3e83a89
Nikk45 <nicksin36@gmail.com>
2023-07-28 15:35:54 +0530

    chat app

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+// make an express app
+const app = express();  
+
+
+// console.log(app);
+
+// making server using http and express;
+const server = require('http').Server(app);
+
+// it will pick index.html from public folder
+app.use(express.static('public'));
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket)=>{
+    console.log('connection established', socket.id);
+
+    // socketA -> io -> socketB
+    // socketA user is triggering a message event
+    socket.on('message',(data)=>{ // user is sending message
+        // 
+        io.emit('message',data); // emitting this message to
+        // all other sockets
+    })
+    // showing off that user left the chat
+    socket.on('disconnect',()=>{
+        console.log(socket.id,'->left the chat');
+    })
+})
+
+
+const PORT = 9000;
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
+
